d469bc0ac464b736f2bd33452b1addafe02e3a46
ANUSRIRAMAN <anusriraman55@gmail.com>
2022-12-30 21:49:04 +0530

    change

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'store'),
+      database: env('DATABASE_NAME', process.env.database),
       user: env('DATABASE_USERNAME', process.env.user),
       password: env('DATABASE_PASSWORD', process.env.password),
       ssl: env.bool('DATABASE_SSL', false),
